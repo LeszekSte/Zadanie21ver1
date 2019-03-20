e518dc4bef6071d605a2f5231c74731ca079ee7c
Leszek <leszekstepien@o2.pl>
2019-03-20 20:49:02 +0100

    Start

---
+++ src/main/java/pl/ls/zad21ver1/Zad21ver1Application.java
@@ -0,0 +1,13 @@
+package pl.ls.zad21ver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad21ver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad21ver1Application.class, args);
+    }
+
+}
